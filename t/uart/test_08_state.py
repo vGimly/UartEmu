cfd204ba4cd051ff951f32c5fdd663da0559b0c2
Vitaly Gimly <vgimly@ya.ru>
2026-09-04 18:55:00 +0300

    cmd

--- t/uart/test_08_state.py
+++ t/uart/test_08_state.py
@@ -10,18 +10,19 @@ async def test_param_02_set_get():
 
     try:
         value = random.randint(-512, 511)
+        cmd = 0x02
 
         await uart_set(
             reader,
             writer,
-            0x02,
+            cmd,
             value,
         )
 
         got = await uart_get(
             reader,
             writer,
-            0x02,
+            cmd,
         )
 
         assert got == str(value).encode("ascii")
